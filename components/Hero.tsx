8c5bc7f46d492ccdd9ae78d8cd0cd52d9afc7808
omer aydin <omeraydin2112@gmail.com>
2023-11-04 19:01:44 +0300

    hero and public assets added

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -1,3 +1,5 @@
+"use client"
+
 import React from 'react'
 import Image from 'next/image'
 import  CustomButton from './CustomButton'
@@ -27,6 +29,16 @@ const Hero = () => {
                 containerStyles = "bg-primary-blue text-white rounded-full mt-10"
                 handleClick={handleScroll }
                 />
+        </div>
+        <div className='hero__image-container'>
+          <div className='hero__image'>
+            <Image src="/hero.png" alt='hero' 
+            fill className='object-contain' />
+
+            <div className='hero__image-overlay'>
+
+            </div>
+          </div>
 
         </div>
     </div>
