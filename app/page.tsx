dfc59a39e74b5e95b02b736a462843de53fc5abc
omer aydin <omeraydin2112@gmail.com>
2023-11-05 05:54:41 +0300

    bug fix

--- app/page.tsx
+++ app/page.tsx
@@ -22,8 +22,8 @@ export default function Home() {
           <SearchBar />
 
           <div className='home__filter-container'>
-            <CustomFilter title="fuel"/>
-            <CustomFilter title="year"/>
+            {/* <CustomFilter title="fuel"/>
+            <CustomFilter title="year"/> */}
           </div>
         </div>
 
